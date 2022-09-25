67b24f12d26add4684559ba15887c58808e1d44e
Mateusz Myśliwiec <mateuszmysliwiec03@gmail.com>
2022-09-25 23:24:48 +0200

    Added main page without working buttons and images. This its totaly basic side design

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,4 +1,82 @@
 import "./main.css";
+import { ReactComponent as Add } from "../../assets/image/add.svg";
+import { ReactComponent as Open } from "../../assets/image/open.svg";
+import { ReactComponent as Trash } from "../../assets/image/delete.svg";
 
-const Main = () => {};
+const Main = () => {
+  return (
+    <div className="mainContainer">
+      <header>
+        <h1 className="nick">Welcome,John Doe</h1>
+        <div className="resetTasks">Reset</div>
+        <div className="logout">Logout</div>
+      </header>
+      <main>
+        <nav>
+          <h1 className="taskTitle">To Do</h1>
+          <ol>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+          </ol>
+          <Add className="addImg" />
+        </nav>
+        <nav>
+          <h1 className="taskTitle">In progress</h1>
+          <ol>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+          </ol>
+          <Add className="addImg" />
+        </nav>
+        <nav>
+          <h1 className="taskTitle">Ended</h1>
+          <ol>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+            <li>
+              Task
+              <Open className="open" />
+              <Trash className="trash" />
+            </li>
+          </ol>
+          <Add className="addImg" />
+        </nav>
+      </main>
+    </div>
+  );
+};
 export default Main;
