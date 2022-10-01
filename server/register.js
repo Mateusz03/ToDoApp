821c89c715d9e3c96e143e1e9d77f9f7f85ec177
Mateusz Myśliwiec <mateuszmysliwiec03@gmail.com>
2022-10-01 17:32:37 +0200

    I changed main site style

--- server/register.js
+++ server/register.js
@@ -1,15 +1,14 @@
 const { usersRef } = require("./firebase");
 
 module.exports.Register = async (params) => {
-
-  const query = await usersRef.where('username','==',params.username).get();
-    if(query.empty){
-      usersRef.add({
-        username:params.username,
-        password:params.password,
-      });
-      return{register:true};
-    }
-    return{register:false};
-  };
-  
+  const query = await usersRef.where("username", "==", params.username).get();
+  if (query.empty) {
+    usersRef.add({
+      username: params.username,
+      password: params.password,
+      ToDo: [],
+    });
+    return { register: true };
+  }
+  return { register: false };
+};
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,85 +1,37 @@
-import { ReactComponent as Add } from "../../assets/image/add.svg";
-import { ReactComponent as Open } from "../../assets/image/open.svg";
+import { ReactComponent as Done } from "../../assets/image/done.svg";
 import { ReactComponent as Trash } from "../../assets/image/delete.svg";
-import { ReactComponent as Restart } from "../../assets/image/restart.svg";
-import { getToken } from "../others/token";
-import TextEditor from "../editor/editor";
+import { getToken, setToken } from "../others/token";
 import "./main.css";
 const Main = () => {
   return (
     <div className="mainContainer">
       <header>
         <h1 className="nick">Welcome, {getToken().username}</h1>
-        <div className="resetTasks">
-          <Restart />
+        <div
+          className="logout"
+          onClick={() => {
+            setToken({ token: false });
+            window.location.reload(true);
+          }}
+        >
+          Logout
         </div>
-        <div className="logout">Logout</div>
       </header>
-      <TextEditor />
       <main>
-        <nav>
-          <h1 className="taskTitle">To Do</h1>
-          <ol>
-            <li>
-              Task
-              <Open className="open" />
+        <div>
+          <nav>
+            <div className="task"></div>
+            <div className="taskButtons">
+              <Done className="done" />
               <Trash className="trash" />
-            </li>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-          </ol>
-          <Add className="addImg" />
-        </nav>
-        <nav>
-          <h1 className="taskTitle">In progress</h1>
-          <ol>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-          </ol>
-          <Add className="addImg" />
-        </nav>
-        <nav>
-          <h1 className="taskTitle">Ended</h1>
-          <ol>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-            <li>
-              Task
-              <Open className="open" />
-              <Trash className="trash" />
-            </li>
-          </ol>
-          <Add className="addImg" />
-        </nav>
+            </div>
+          </nav>
+        </div>
+        <div className="createToDo">
+          <p className="ToDoTitle">What you need to do?</p>
+          <textarea wrap="soft" type="text" className="ToDoInput" />
+          <div className="doneButton">Done</div>
+        </div>
       </main>
     </div>
   );
